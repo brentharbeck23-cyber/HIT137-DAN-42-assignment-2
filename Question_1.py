1ed96085ea2b2952bd5ed37655fb47ccb0ea4eb4
Meuli92 <78645658+Meuli92@users.noreply.github.com>
2026-04-09 12:43:34 +0800

    added notes, this is a test of github

--- Question_1.py
+++ Question_1.py
@@ -4,6 +4,7 @@ This program will encrypt and decrypt a given .txt file
 
 '''
 
+#Define the encrypt function
 def encrypt(text, shift1, shift2):
     result = ''
     for char in text:
@@ -11,16 +12,25 @@ def encrypt(text, shift1, shift2):
             if char.islower():
                 base = ord('a')
                 if ord(char) >= 97 and ord(char) <= 109:
+                    #need to change this to %13 so that letters stay in their half of thhe alphabet
                     result += chr((ord(char) - base + shift1 * shift2) % 26 + base)
                 else: result += chr((ord(char) - base - shift1 - shift2) % 26 + base)
             elif char.isupper():
                 base = ord('A')
                 if ord(char) >= 65 and ord(char) <= 77:
+                    #same here, change this to %13
                     result += chr((ord(char) - base - shift1) % 26 + base)
                 else: result += chr((ord(char) - base + shift2 * shift2) % 26 + base)
         else:
             result += char
     return result
+
+#Define the decrypt function
+def decrypt(text_encrypt, shift1, shift2):
+    result = ''
+    return result
+
+#Main program body (run 'encrypt' then write the txt file or run 'decrypt' then write the text file and verify)
 
 text = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 shift1 = 2
