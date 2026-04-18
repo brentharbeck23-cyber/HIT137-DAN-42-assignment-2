72ee5f50bf0f336e0c36d898d605d0f99556cfd5
Jacob Meuli <Jacob@Jacobs-MacBook-Air.local>
2026-04-19 06:59:54 +0800

    exit after error message and code cleaned up

--- Question_1.py
+++ Question_1.py
@@ -5,6 +5,7 @@ This program will encrypt and decrypt a given .txt file
 '''
 
 #Define the encrypt function
+
 def encrypt(text, shift_1, shift_2):
     result = ''
     for char in text:
@@ -13,19 +14,36 @@ def encrypt(text, shift_1, shift_2):
                 base_a_lower = ord('a')
                 base_n_lower = ord('n')
                 if ord(char) >= 97 and ord(char) <= 109:
-                    result += chr((ord(char) - base_a_lower + shift_1 * shift_2) % 13 + base_a_lower)
-                else: result += chr((ord(char) - base_n_lower - shift_1 - shift_2) % 13 + base_n_lower)
+                    result += chr((ord(char)
+                                   - base_a_lower
+                                   + shift_1
+                                   * shift_2) % 13
+                                   + base_a_lower)
+                else:
+                    result += chr((ord(char)
+                                   - base_n_lower
+                                   - shift_1
+                                   - shift_2) % 13
+                                   + base_n_lower)
             elif char.isupper():
                 base_a_upper = ord('A')
                 base_n_upper = ord('N')
                 if ord(char) >= 65 and ord(char) <= 77:
-                    result += chr((ord(char) - base_a_upper - shift_1) % 13 + base_a_upper)
-                else: result += chr((ord(char) - base_n_upper + shift_2 ** 2) % 13 + base_n_upper)
+                    result += chr((ord(char)
+                                   - base_a_upper
+                                   - shift_1) % 13
+                                   + base_a_upper)
+                else:
+                    result += chr((ord(char)
+                                   - base_n_upper
+                                   + shift_2 ** 2) % 13
+                                   + base_n_upper)
         else:
             result += char
     return result
 
 #Define the decrypt function
+
 def decrypt(text_encrypt, shift_1, shift_2):
     result_decrypt = ''
     for char in text_encrypt:
@@ -34,14 +52,29 @@ def decrypt(text_encrypt, shift_1, shift_2):
                 base_a_lower = ord('a')
                 base_n_lower = ord('n')
                 if ord(char) >= 97 and ord(char) <= 109:
-                    result_decrypt += chr((ord(char) - base_a_lower - shift_1 * shift_2) % 13 + base_a_lower)
-                else: result_decrypt += chr((ord(char) - base_n_lower + shift_1 + shift_2) % 13 + base_n_lower)
+                    result_decrypt += chr((ord(char)
+                                           - base_a_lower
+                                           - shift_1
+                                           * shift_2) % 13
+                                           + base_a_lower)
+                else:
+                    result_decrypt += chr((ord(char)
+                                           - base_n_lower
+                                           + shift_1 + shift_2) % 13
+                                           + base_n_lower)
             elif char.isupper():
                 base_a_upper = ord('A')
                 base_n_upper = ord('N')
                 if ord(char) >= 65 and ord(char) <= 77:
-                    result_decrypt += chr((ord(char) - base_a_upper + shift_1) % 13 + base_a_upper)
-                else: result_decrypt += chr((ord(char) - base_n_upper - shift_2 ** 2) % 13 + base_n_upper)
+                    result_decrypt += chr((ord(char)
+                                           - base_a_upper
+                                           + shift_1) % 13
+                                           + base_a_upper)
+                else:
+                    result_decrypt += chr((ord(char)
+                                             - base_n_upper
+                                             - shift_2 ** 2) % 13
+                                             + base_n_upper)
         else:
             result_decrypt += char
     return result_decrypt
@@ -58,19 +91,23 @@ def write_file(file_name: str, content: str) -> None:
     with open(file_name, 'w', encoding='utf-8') as file:
         return file.write(content)
 
-#Main program body (run 'encrypt' then write the txt file or run 'decrypt' then write the text file and verify)
+#Main program body (run 'encrypt' then write the txt file, then run 'decrypt'
+# and write the text file then verify)
 
 text = read_file('raw_text.txt')
 shift_1 = int(input('Choose a number from 1 to 12 > '))
 shift_2 = int(input('Choose another number from 1 to 12 > '))
-# Check that the number is between 1 and 12 and run the encrypt function
+# Check that the number is between 1 and 12 and run the encrypt function or print error then exit
 if 0 < shift_1 < 13 and 0 < shift_2 < 13:
     text_encrypt = (encrypt(text,shift_1,shift_2))
     write_file('encrypted_text.txt',text_encrypt)
     text_decrypt = decrypt(read_file('encrypted_text.txt'),shift_1,shift_2)
     write_file('decrypted_text.txt',text_decrypt)
-else: print('Error')
-
+else:
+    print('Error')
+    exit()
+#Check that the two text files match
 if read_file('decrypted_text.txt') == read_file('raw_text.txt'):
     print('Your decryption was successful!')
-else: print('Your decryption was unsuccessful :/')
+else:
+    print('Your decryption was unsuccessful :/')
